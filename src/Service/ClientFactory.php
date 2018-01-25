63ccd308b602fc31fe0631254258d036c8517c2a
Davide Borsatto <davide.borsatto@gmail.com>
2018-01-25 15:37:26 +0100

    feat(host): Add customizable host setting

--- src/Service/ClientFactory.php
+++ src/Service/ClientFactory.php
@@ -31,13 +31,27 @@ class ClientFactory
     private $cacheDir;
 
     /**
+     * @var string
+     */
+    private $deliveryApiUrl;
+
+    /**
+     * @var string
+     */
+    private $previewApiUrl;
+
+    /**
      * @param State  $state
      * @param string $cacheDir
+     * @param string $deliveryApiUrl
+     * @param string $previewApiUrl
      */
-    public function __construct(State $state, string $cacheDir)
+    public function __construct(State $state, string $cacheDir, string $deliveryApiUrl, string $previewApiUrl)
     {
         $this->state = $state;
         $this->cacheDir = $cacheDir.'/contentful';
+        $this->deliveryApiUrl = $deliveryApiUrl;
+        $this->previewApiUrl = $previewApiUrl;
     }
 
     /**
@@ -66,12 +80,25 @@ class ClientFactory
                 : $this->state->getPreviewToken()
         );
 
+        // WARNING:
+        // This is not needed during normal use.
+        // URLs are determined automatically from the Client,
+        // but here we need to make them configurable for internal purposes.
+        // Overall, you might need the `uriOverride` option (for instance)
+        // for setting up a custom proxy, but for most cases this is not something
+        // you should care about too much.
+        // This means that in normal use, we wouldn't have environment variables
+        // for defining these URLs, and they wouldn't be injected in this factory.
+        $url = Contentful::API_DELIVERY === $api
+            ? $this->deliveryApiUrl
+            : $this->previewApiUrl;
+
         $client = new Client(
             $accessToken,
             $spaceId,
             Contentful::API_PREVIEW === $api,
             $this->state->getLocale(),
-            ['cacheDir' => $this->cacheDir]
+            ['cacheDir' => $this->cacheDir, 'uriOverride' => $url]
         );
         $client->setApplication(Kernel::APP_NAME, Kernel::APP_VERSION);
 
